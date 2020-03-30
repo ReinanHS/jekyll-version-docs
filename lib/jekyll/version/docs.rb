f006233f81caa326dff2717700bad76189c7168d
ReinanHS <reinangabriel1520@gmail.com>
2020-03-30 16:21:50 -0300

    First functional version

---
+++ lib/jekyll/version/docs.rb
@@ -0,0 +1,10 @@
+require "jekyll/version/docs/version"
+
+module Jekyll
+  module Version
+    module Docs
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/jekyll/version/docs/version.rb
@@ -0,0 +1,7 @@
+module Jekyll
+  module Version
+    module Docs
+      VERSION = "0.1.0"
+    end
+  end
+end
